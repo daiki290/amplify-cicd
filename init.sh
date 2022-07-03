e4c1e3dccb5ac05427b2f47cda989fd27c219397
keid131 <keid951022@gmail.com>
2022-07-03 20:30:15 +0900

    fix

--- init.sh
+++ init.sh
@@ -10,25 +10,35 @@ AWSCLOUDFORMATIONCONFIG="{\
 \"secretAccessKey\":\"${AMPLIFY_CICD_SECRET_KEY}\",\
 \"region\":\"ap-northeast-1\"\
 }"
+
 AMPLIFY="{\
 \"projectName\":\"amplify-cicd\",\
 \"envName\":\"dev\",\
 \"defaultEditor\":\"code\"\
 }"
-VUECONFIG ="{\ 
+
+# VUECONFIG ="{\ 
+# \” SourceDir \”：\” src \”、\ 
+# \” DistributionDir \”：\” build \”、\ 
+# \” BuildCommand \”：\” npm run-script build \”、\ 
+# \” StartCommand \”：\” npm run-script start \” \ 
+# }"
+
+FRONTEND="{\
+\” frontend \”：\” javascript \”、\ 
+\” framework \”：\” vue \”、\ 
+\” config \”：{\ 
 \” SourceDir \”：\” src \”、\ 
 \” DistributionDir \”：\” build \”、\ 
 \” BuildCommand \”：\” npm run-script build \”、\ 
 \” StartCommand \”：\” npm run-script start \” \ 
+}\ 
 }"
-FRONTEND="{\
-\” frontend \”：\” javascript \”、\ 
-\” framework \”：\” vue \”、\ 
-\” config \”：$VUECONFIG \ 
-}"
+
 PROVIDERS="{\
 \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
 }"
+
 CODEGEN="{\
 \”generateCode\”:true,\
 \”codeLanguage\”:\”javascript\”,\
