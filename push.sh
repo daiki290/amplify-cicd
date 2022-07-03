a77d6ecde1fefd60adf8a6e3a14b6ad64625fdc7
keid131 <keid951022@gmail.com>
2022-07-03 20:58:19 +0900

    fix push

--- push.sh
+++ push.sh
@@ -3,12 +3,12 @@ set -e
 IFS='|'
 
 CODEGEN="{\
-\”generateCode\”:true,\
-\”codeLanguage\”:\”javascript\”,\
-\”fileNamePattern\”:\”src/graphql/**/*.js\”,\
-\”generatedFileName\”:\”API\”,\
-\”generateDocs\”:true\
-}”
+\"”generateCode\":true,\
+\"codeLanguage\":\"typescript\",\
+\"fileNamePattern\":\"src/graphql/**/*.ts\",\
+\"generatedFileName\":\"API\",\
+\"generateDocs\":true\
+}"
 
 amplify push \
 --codegen $CODEGEN \
